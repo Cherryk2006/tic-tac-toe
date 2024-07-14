78ef2bdf27c77d771e8ffa4b8e194a3a0c53026b
Cherryk2006 <anastasya.cherednyk@gmail.com>
2024-07-14 12:50:48 -0500

    Add files via upload

---
+++ 8*8.py
@@ -0,0 +1,75 @@
+
+import telebot
+from telebot import types
+import random
+
+API_TOKEN = "your-token"
+bot = telebot.TeleBot('6773691989:AAFtKmympPtdYCMMO_pZp5B7EHo3LkgoZIU')
+
+class Game:
+    def __init__(self, user_x, user_o, message_id):
+        self.root = [[0] * 8 for _ in range(8)]
+        self.user_x = user_x
+        self.user_o = user_o
+        self.current_step = "x"
+        self.game_over = False
+        self.message_id = message_id
+
+    def check_win(self):
+        def check_line(line):
+            count_x = 0
+            count_o = 0
+            for cell in line:
+                if cell == 1:
+                    count_x += 1
+                    count_o = 0
+                elif cell == 2:
+                    count_o += 1
+                    count_x = 0
+                else:
+                    count_x = 0
+                    count_o = 0
+
+                if count_x == 5:
+                    return 1
+                if count_o == 5:
+                    return 2
+            return 0
+
+
+        # Check rows and columns
+        for i in range(5):
+            if check_line(self.root[i]) in [1, 2]:
+                return check_line(self.root[i])
+            if check_line([self.root[j][i] for j in range(8)]) in [1, 2]:
+                return check_line([self.root[j][i] for j in range(8)])
+
+
+        # Check diagonals
+        for i in range(2):
+            if check_line([self.root[i + j][j] for j in range(5 - i)]) in [1, 2]:
+                return check_line([self.root[i + j][j] for j in range(8 - i)])
+            if check_line([self.root[j][i + j] for j in range(5 - i)]) in [1, 2]:
+                return check_line([self.root[j][i + j] for j in range(8 - i)])
+            if check_line([self.root[5 - (i + j)][j] for j in range(8 - i)]) in [1, 2]:
+                return check_line([self.root[5 - (i + j)][j] for j in range(8 - i)])
+            if check_line([self.root[5 - j][i + j] for j in range(8 - i)]) in [1, 2]:
+                return check_line([self.root[5 - j][i + j] for j in range(5 - i)])
+
+        return 0
+
+    def make_move(self, x, y):
+        if self.game_over or self.root[x][y] != 0:
+            return False
+        if self.current_step == "x":
+            self.root[x][y] = 1
+            self.current_step = "o"
+        else:
+            self.root[x][y] = 2
+            self.current_step = "x"
+        if self.check_win():
+            self.game_over = True
+        return True
+
+
+bot.infinity_polling()
